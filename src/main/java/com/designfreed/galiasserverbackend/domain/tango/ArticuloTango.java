dc440936d7c7ac0756bdaef49a97df1bb8e4c36e
mbisurgi <maximiliano@designfreed.com>
2018-02-26 15:08:41 -0300

    Agregar articulos, listas y precios

--- src/main/java/com/designfreed/galiasserverbackend/domain/tango/ArticuloTango.java
+++ src/main/java/com/designfreed/galiasserverbackend/domain/tango/ArticuloTango.java
@@ -3,21 +3,80 @@ package com.designfreed.galiasserverbackend.domain.tango;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "STA11")
 public class ArticuloTango implements Serializable {
+    @TableGenerator(name = "articulo-gen", table = "PROVIDERID", pkColumnName = "ID_PROVIDERID", valueColumnName = "ULTIMOID", pkColumnValue = "29", allocationSize=1)
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "articulo-gen")
     @Column(name = "ID_STA11")
     private Long id;
 
     @Column(name = "COD_ARTICU")
     private String codigo;
 
+    @Column(name = "COD_II")
+    private Integer iiVentas;
+
+    @Column(name = "COD_II_CO")
+    private Integer iiCompras;
+
+    @Column(name = "COD_IVA")
+    private Integer ivaVentas;
+
+    @Column(name = "COD_IVA_CO")
+    private Integer ivaCompras;
+
+    @Column(name = "COD_S_II")
+    private Integer percepIiVentas;
+
+    @Column(name = "COD_S_II_C")
+    private Integer percepIiCompras;
+
+    @Column(name = "COD_S_IVA")
+    private Integer percepIvaVentas;
+
+    @Column(name = "COD_S_IV_C")
+    private Integer percepIvaCompras;
+
     @Column(name = "DESCRIPCIO")
     private String descripcion;
+
+    @Column(name = "EQUIVALE_V")
+    private Float equivalenciaVenta;
+
+    @Column(name = "FECHA_ALTA")
+    private Date fechaAlta;
+
+    @Column(name = "PERFIL")
+    private String perfil;
+
+    @Column(name = "STOCK")
+    private Boolean stock;
+
+    @Column(name = "USA_ESC")
+    private String escala;
+
+    @Column(name = "USA_PARTID")
+    private Boolean partida;
+
+    @Column(name = "REMITIBLE")
+    private String remitible;
+
+    @Column(name = "COD_STA11")
+    private String codigoTabla11;
+
+    @Column(name = "ID_MEDIDA_STOCK")
+    private Integer unidadStock;
+
+    @Column(name = "ID_MEDIDA_VENTAS")
+    private Integer unidadVenta;
+
+    @Column(name = "DESCARGA_NEGATIVO_STOCK")
+    private Boolean stockNegativo;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
     @JoinColumn(name = "COD_ARTICU", referencedColumnName = "COD_ARTICU", nullable = false)
---
+++ src/main/java/com/designfreed/galiasserverbackend/domain/tango/ListaPrecioItemTango.java
@@ -0,0 +1,68 @@
+package com.designfreed.galiasserverbackend.domain.tango;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "GVA17")
+public class ListaPrecioItemTango {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID_GVA17")
+    private Long id;
+
+    @Column(name = "COD_ARTICU")
+    private String articulo;
+
+    @Column(name = "NRO_DE_LIS")
+    private Integer numero;
+
+    @Column(name = "PRECIO")
+    private Double precio;
+
+    @Column(name = "FECHA_MODI")
+    private Date fechaModificacion;
+
+    public ListaPrecioItemTango() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getArticulo() {
+        return articulo;
+    }
+
+    public void setArticulo(String articulo) {
+        this.articulo = articulo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Date getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    public void setFechaModificacion(Date fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+}
